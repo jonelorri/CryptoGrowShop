90df97aa88b5fb1559f6629268a819a2cb81a9b5
Jon Elorriaga <jonelorriaga@iMac-de-Jon.local>
2022-07-27 13:14:33 +0200

    text-typing

--- src/First.jsx
+++ src/First.jsx
@@ -2,9 +2,24 @@ import style from './First.module.css';
 import background from './img/background.jpg';
 
 export function First () {
+    let i = 0;
+    let text = "BUY YOUR SEEDS WITH CRYPTO";
+
+    function typing() {
+        if (i < text.length) {
+            document.querySelector('.title').innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typing, 100)
+        }
+    }
+
+    document.addEventListener('readystatechange', event => {
+        typing();
+    });
+
     return(
         <div style={{backgroundImage: `url(${background})`}} className={style.container}>
-            <h1>BUY YOUR SEEDS WITH CRYPTO</h1>
+            <h1 className='title'></h1>
             <h2>Do it the easy way</h2>
             <div className={style.gradient}></div>
         </div>
